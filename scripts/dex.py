e956a81f912cdd4549bebc2b18e42dc7ad2ff1f0
OZ <abuzaid.sahab.2015@gmail.com>
2023-04-08 17:56:07 +0300

    adding template files

---
+++ scripts/dex.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import time
+from brownie import Dex, DexToken, SwappableToken  # type: ignore
+from scripts.scripts import get_account, get_contract, get_details
+from web3 import Web3 as web3
+
+
+def prepare(contract, token1, token2, owner, account=get_account()):
+    if contract.token1() == token1.address and contract.token2() == token2.address:
+
+        contract.setTokens(token1.address, token2.address, {"from": owner})
+    if int(token1.balanceOf(contract.address)) < 100:
+        token1.transfer(contract.address, 100, {"from": owner})
+    if int(token2.balanceOf(contract.address)) < 100:
+        token2.transfer(contract.address, 100, {"from": owner})
+    if int(token1.balanceOf(account.address)) != 10:
+        token1.transfer(account.address, 10, {"from": owner})
+    if int(token2.balanceOf(account.address)) != 10:
+        token2.transfer(account.address, 10, {"from": owner})
+    return
+
+
+def main():
+    account = get_account()
+    owner = get_account(1)  # only in development returns accounts[1]
+    dex = get_contract(Dex, account=owner)  # (contract,args)
+    print("dex: ", dex)
+    print("dex address: ", dex.address)
+    token1 = get_contract(
+        SwappableToken, dex.address, "Monate", "MET", 1000, account=owner)  # (contract,args)
+    token2 = get_contract(
+        SwappableToken, dex.address, "Valdoz", "VOZ", 1000, account=owner)  # (contract,args)
+    swap_amount = 10
+    attrs = {
+        "schema": {"function_name": ["arg1", "arg2"]},
+        "dex": [
+            {"balanceOf": [token1.address, account.address]},
+            {"balanceOf": [token2.address, owner.address]},
+            {"getSwapPrice": [token1.address, token2.address, swap_amount]}
+        ]
+    }
+    dex.setTokens(token1.address, token2.address, {"from": owner})
+
+    # the implementation is not part of the template
+    prepare(dex, token1, token2, owner, account=account)
+
+    get_details(dex, attrs['dex'], account=owner)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/scripts.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from brownie import accounts, config, Contract, network  # type: ignore
+LOCAL_NETWORKS = ["development", 'ganache-local', 'private']
+
+def get_account(n=0):
+    if network.show_active() in LOCAL_NETWORKS:
+        return accounts[n]
+    else:
+        return accounts.add(config['wallets']['from'])
+
+
+def get_contract(contract, *args, account=get_account(), contract_address="", contract_name="Monate", _deploy=False):
+    if _deploy:
+        deploy(contract,account, *args)
+    elif (network.show_active() in LOCAL_NETWORKS):
+        try:
+            return contract[-1]
+        except:
+            return deploy(contract,account, *args)
+    else:
+        if not contract_address:
+            raise Exception("Address can't be null")
+        abi = contract.abi
+        return Contract.from_abi(contract_name, contract_address, abi)
+
+
+def deploy(contract,account, *args):
+    if len(args) == 0:
+        return contract.deploy({"from": account})
+    return contract.deploy(*args, {"from": account})
+
+
+def get_details(contract, args, account=get_account()):
+    for attr in args:
+        for function_name, args in attr.items():
+            function = getattr(contract, function_name)
+            result = function(*args, {"from": account})
+            _args = [str(arg) for arg in args]
+            print(
+                f"function {function_name}({','.join(_args)}) returned {result}")
